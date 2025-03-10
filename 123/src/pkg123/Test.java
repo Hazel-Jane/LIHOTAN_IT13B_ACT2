a87c91c4a5742fe4683d0f0d2e2f78c418d4f897
LutragoacliffordIT24A <cliffordjohnburdeoslutrago@gmail.com>
2025-03-10 11:04:41 +0800

    added midterm palindrome act1

---
+++ 123/src/pkg123/Test.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg123;
+
+/**
+ *
+ * @author User
+ */
+ import java.util.ArrayList;
+public class Test {
+    public static void main (string[]args){
+        
+        ArrayList<String> cars = new ArrayList<String>();
+        
+        cars.add("MissyouBibi");
+        cars.add("Honda");
+        cars.add("civic");
+        
+        System.out.println(cars);
+        
+        
+        
+    }
+    
+}
---
+++ arrays/src/arrays/Arrays.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arrays;
+
+/**
+ *
+ * @author User
+ */
+import java.util.Arrays;
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("Array elements:");
+        for (int i=0; i < numbers.length; i++){
+            System.out.println("Elements at index" +i+ ": "+ numbers[i]);
+        }
+     }
+    
+}
