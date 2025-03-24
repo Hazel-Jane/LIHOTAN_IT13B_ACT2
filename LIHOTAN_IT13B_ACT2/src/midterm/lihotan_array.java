efdefe7be8ed456cedb0cb5c3c4e4081982a49d1
Junessa19 <hjlihotan@gmail.com>
2025-03-24 11:04:17 +0800

    added single and multi array mid

---
+++ LIHOTAN_IT13B_ACT2/src/midterm/lihotan_array.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midterm;
+
+/**
+ *
+ * @author User
+ */
+public class lihotan_array {
+    public static void main(String[] args) {
+        
+        // SINGLE ARRAY
+        char letters[]= {'A','B','C','D','E','F','G'}; 
+        
+        for (char l = 0; l < letters.length; l++) {
+            System.out.print(letters[l] + " , ");
+          
+        }
+  
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
